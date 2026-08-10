bd43b8d4ea8a03ff2db7c094af3516f2d6aed9ff
Jashanpreet singh <jashar2113@GMAIL.COM>
2026-08-10 12:31:12 +0530

    Add files via upload

---
+++ jashan21.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+
+int main() {
+    int a[] = {10, 5, 20, 8, 1};
+    int n = 5, max, second;
+
+    max = second = -9999;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > max) {
+          second = max;
+          max =a[i];
+          }
+          else if(a[i]>>second && a[i] != max); 
+          second = a[i];
+          {
+        
+    }
+
+    printf("Second largest") =%d;
+    
