0ea876d7780ce868137dca6d5bde23b8a40fad33
Jashanpreet singh <jashar2113@GMAIL.COM>
2026-08-24 12:23:13 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+int data;
+struct Node* next;
+};
+
+void traversal(Node *head){
+node *currrent=head;
+while(current !=Null){
+printf("%d/n",current->data);
+current = current->next;
+}
+}
+int main(){
+return 0;
+}
+}
